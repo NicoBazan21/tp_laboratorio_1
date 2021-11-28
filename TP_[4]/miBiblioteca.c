3da65a50147b84f0cf5c0e8da1c0e4414461a224
NicoBazan21 <86837081+NicoBazan21@users.noreply.github.com>
2021-11-28 12:09:51 -0300

    Sin corregir todavia

--- TP_[4]/miBiblioteca.c
+++ TP_[4]/miBiblioteca.c
@@ -61,42 +61,30 @@ int menuDos()
 
 int validarNumCadenas(char* name, int tam)
 {
-    int todoOk= 0;
-    int okey;
-    int error = 1;
+   int okey;
+    int error = 0;
 
     if(name != NULL && tam > 0)
     {
-        do{
             error=0;
             for(int i = 0; i < tam; i++)
             {
                 okey = isalpha(name[i]);
-
-                if(name[i] == ' ')
-                {
-                    okey=1;
-                }
                 if(okey == 0)
                 {
-                    printf("Error. Ingreso con caracteres invalidos. Ingrese nuevamente: ");
-                    gets(name);
-                    tam=strlen(name);
                     error = 1;
                 }
             }
 
-        }while(error == 1);
-
-        todoOk=1;
     }
-    return todoOk;
+    return error;
 }
 int pedirCadena(char cadena[], int tam, char mensajeUno[], char mensajeError[])
 {
-    int todoOk =0;
+   int todoOk =0;
     char entrada[256];
     int tamEntrada;
+    int error;
 
     if(cadena != NULL && tam > 0 && mensajeUno != NULL && mensajeError != NULL)
     {
@@ -105,21 +93,20 @@ int pedirCadena(char cadena[], int tam, char mensajeUno[], char mensajeError[])
         gets(entrada);
         tamEntrada = strlen(entrada);
 
-        validarNumCadenas(entrada, tamEntrada);
+        error=validarNumCadenas(entrada, tamEntrada);
 
-        while(tamEntrada > tam || tamEntrada < 1)
+        while(tamEntrada > tam || tamEntrada <= 2 || error == 1)
         {
             printf("%s", mensajeError);
             fflush(stdin);
             gets(entrada);
             tamEntrada=strlen(entrada);
+            error=validarNumCadenas(entrada, tamEntrada);
         }
+
 
         strcpy(cadena, entrada);
         todoOk=1;
-    }
-
-    return todoOk;
 }
 
 int pedirFlotante(float* numero, char mensajeUno[], char mensajeError[], float min, float max)
